74e1565683b74da58f40ce35855ed18c23e08ef7
TB831 <druu831@gmail.com>
2019-01-23 14:41:27 -0800

    icons display depending on season

--- src/Components/SeasonDisplay.js
+++ src/Components/SeasonDisplay.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.latitude, new Date().getMonth());
+    const displayText = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
+    const iconImage = season === 'winter' ? 'snowflake' : 'sun';
+
     return (
-        <div>{season}</div>
+        <div>
+            <i className={`${iconImage} icon`} />
+            <h1>{displayText}</h1>
+            <i className={`${iconImage} icon`} />
+        </div>
     )
 }
 
-const getSeason = (latitude, month) => {
+const getSeason = (latitude, month) => {    // Helper function to determine winter/summer depending on lat location
     if (month > 2 && month < 9) {
         return latitude > 0 ? 'summer' : 'winter'
     } else {
